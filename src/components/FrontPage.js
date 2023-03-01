2c4af9a268089af1b8cd686dbce077000565814c
Sudeep Kumar <sudeepagrawal1999@gmail.com>
2023-03-01 15:11:13 +0530

    ui functionality added to register page

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -4,7 +4,7 @@ export const FrontPage = () => {
   return (
     <div className="front-page-container relative flex flex-1 flex-col items-center justify-center">
       <div className="image-logo">
-        <img src={logo} alt="logo" srcset="" />
+        <img src={logo} alt="logo"/>
       </div>
       <div className="frontpage relative block bg-white text-8xl text-black font-bold">
         Demo Project in React
--- src/components/SignUpComponent.js
+++ src/components/SignUpComponent.js
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 export const SignUpComponent = () => {
   const [registerState, setregisterState] = useState({
-    email: "",
+    email_signup: "",
     password_register: "",
-    confirmPassword: "",
-    ispasswordChecked: false,
-    isconfirmPasswordChecked: false,
+    confirm_password_register: "",
+    show_pass_register: false,
+    show_pass_register_confirm: false,
   });
+
+  const changeHandler = useCallback((e) => {
+    setregisterState((previousState) => {
+      return { ...previousState, [e.target.name]: e.target.value };
+    });
+  }, []);
+
+  const checkHandler = useCallback((e) => {
+    setregisterState((previousState) => {
+      return { ...previousState, [e.target.name]: e.target.checked };
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(registerState);
+  }, [registerState]);
+
   return (
     <div className="signup relative flex flex-1 bg-white items-center justify-center">
       <div className="register-body-container">
@@ -20,14 +37,20 @@ export const SignUpComponent = () => {
                 name="email_signup"
                 id="email"
                 placeholder="Email Address"
+                onChange={changeHandler}
               />
             </div>
             <div className="register-password">
               <input
-                type="password"
+                type={
+                  registerState.show_pass_register !== false
+                    ? "text"
+                    : "password"
+                }
                 name="password_register"
                 id="user-password-register"
                 placeholder="Password (minimum 8 characters)"
+                onChange={changeHandler}
               />
             </div>
             <div className="showpassword-register relative flex pl-5">
@@ -42,14 +65,20 @@ export const SignUpComponent = () => {
                 name="show_pass_register"
                 id="show-pass-register"
                 className="relative block mx-4"
+                onChange={checkHandler}
               />
             </div>
             <div className="register-password-again">
               <input
-                type="password"
+                type={
+                  registerState.show_pass_register_confirm !== false
+                    ? "text"
+                    : "password"
+                }
                 name="confirm_password_register"
                 id="user-password-register-again"
                 placeholder="Confirm Password"
+                onChange={changeHandler}
               />
             </div>
             <div className="showpassword-register-confirm relative flex pl-5">
@@ -64,6 +93,7 @@ export const SignUpComponent = () => {
                 name="show_pass_register_confirm"
                 id="show-pass-register-confirm"
                 className="relative block mx-4"
+                onChange={checkHandler}
               />
             </div>
             <div className="user-register-button">
