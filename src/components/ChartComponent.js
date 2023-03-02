80b41ef33e111172ab63c2f17bdbc9013ce2677c
Sudeep Kumar <sudeepagrawal1999@gmail.com>
2023-03-02 14:30:11 +0530

    highcharts done with proper navigation

--- src/components/ChartComponent.js
+++ src/components/ChartComponent.js
@@ -1,8 +1,87 @@
-import React from 'react'
+import React, { useCallback, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Highcharts from "highcharts";
+import "./highcharts.css";
+export const ChartComponent = ({ email }) => {
+  let navigate = useNavigate();
+  const logoutHandler = useCallback(
+    (e) => {
+      let confirmStatus = window.confirm("Are you sure you want to logout?");
+      if (confirmStatus) {
+        localStorage.setItem(`${email}Status`, "false");
+        navigate("/");
+      }
+    },
+    [email, navigate]
+  );
+  useEffect(() => {
+    Highcharts.chart("highcharts", {
+      chart: {
+        type: "pie",
+        options3d: {
+          enabled: true,
+          alpha: 55,
+        },
+      },
+      title: {
+        text: "Demo Donut Chart for Beijing 2022 gold medals by country",
+        align: "center",
+      },
+      subtitle: {
+        text: "3D donut in Highcharts",
+        align: "center",
+      },
+      plotOptions: {
+        pie: {
+          innerSize: 400,
+          depth: 65,
+          cursor: "pointer",
+          dataLabels: {
+            enabled: true,
+            format: "{point.name}<br/>{y}",
+          },
+          showInLegend: true,
+        },
+      },
+      series: [
+        {
+          name: "Medals",
+          data: [
+            ["Norway", 16],
+            ["Germany", 12],
+            ["USA", 8],
+            ["Sweden", 8],
+            ["Netherlands", 8],
+            ["ROC", 6],
+            ["Austria", 7],
+            ["Canada", 4],
+            ["Japan", 3],
+          ],
+        },
+      ],
+    });
+  }, []);
 
-export const ChartComponent = ({email}) => {
-    console.log(email);
   return (
-    <div>hello</div>
-  )
-}
+    <div className="chart_parent_container relative block w-screen">
+      <div className="topbar_chart relative flex h-14 items-center justify-between px-5 bg-green-200">
+        <span className="useremail relative block text-amber-800 text-xl font-semibold">
+          {email}
+        </span>
+        <span
+          className="logout_chart relative block text-red-700 text-xl font-semibold"
+          onClick={logoutHandler}
+        >
+          SignOut
+        </span>
+      </div>
+      <div
+        className="chartcomponents relative flex flex-1 h-[88%]"
+        id="highcharts"
+      ></div>
+      <div className="footer relative flex items-center justify-center text-gray-600">
+        All Rights Reserved ©{email}- 2023
+      </div>
+    </div>
+  );
+};
